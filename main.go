5fa13d36c2cdb399c968ea7ce29d81c1fefc1be4
sleepymanta <124329693+sleepymanta@users.noreply.github.com>
2023-10-06 18:54:05 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -39,10 +39,17 @@ func main() {
 		2000,
 	})
 
+	var vol4 *book = new(book)
+	vol4.isbn = 9781408845684
+	vol4.title = "Harry Potter And The Order Of The Phoenix"
+	vol4.year = 2003
+	harryPotterBook = append(harryPotterBook, *vol4)
+
 	fmt.Println(harryPotterBook)
 	// [{9781408898154 Harry Potter And The Chamber Of Secrets 1998} {978
 	// 1408855911 Harry Potter And The Prisoner Of Azkaban 1999} {9781338
-	// 299175 Harry Potter And The Goblet Of Fire 2000}]
+	// 299175 Harry Potter And The Goblet Of Fire 2000} {9781408845684 Ha
+	// rry Potter And The Order Of The Phoenix 2003}]
 
 	fmt.Println(harryPotterBook[0].year)
 	// 1998
